ae8c2dfb537c9e3e2856290818e24728e50c9e72
Jack Grigg <me@jackgrigg.com>
2011-02-13 09:47:56 +0000

    Modifications to tables-authdjango.sql
    Thanks go to liangent from irc.freenode.net/#mediawiki

--- tables-authdjango.sql
+++ tables-authdjango.sql
@@ -1,9 +1,8 @@
 -- Copyright (C) 2011 Jack Grigg
 -- Table structure for table 'Django User'
--- Replace <PREFIX> with the proper prefix
 
-CREATE TABLE IF NOT EXISTS <PREFIX>_authdjango (
-    'mw_user_id' integer unsigned NOT NULL PRIMARY KEY,
-    'd_user_id' integer unsigned,
-    FOREIGN KEY ('mw_user_id') REFERENCES '<PREFIX>_user' ('user_id')
-    ) ENGINE=InnoDB DEFAULT CHARSET=binary;
+CREATE TABLE IF NOT EXISTS /*_*/authdjango (
+    mw_user_id integer unsigned NOT NULL PRIMARY KEY,
+    d_user_id integer unsigned,
+    FOREIGN KEY (mw_user_id) REFERENCES /*_*/user (user_id)
+    ) /*$wgDBTableOptions*/;
